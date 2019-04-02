f0b1c526c315e41b3620a6342dd624121f871ecf
Romane G <romane71193@gmail.com>
2019-04-02 17:43:41 -0400

    opactiy to css buttons

--- src/components/Page1.js
+++ src/components/Page1.js
@@ -5,6 +5,7 @@ const Page1 = ({onRouteChange}) =>
 
     <div>
       
+      <button className="disabled">Page1</button>
       <button onClick={() => onRouteChange('page2')}>Page2</button>
       <button onClick={() => onRouteChange('page3')}>Page3</button>
     </div>
--- src/components/Page3.js
+++ src/components/Page3.js
@@ -5,6 +5,7 @@ const Page3 = ({onRouteChange}) =>
   
     <div>
       
+      <button className="disabled">Page3</button>
       <button onClick={() => onRouteChange('page1')}>Page1</button>
       <button onClick={() => onRouteChange('page2')}>Page2</button>
     </div>
